fdb16294ad510d121a832e2ee90e18a80f7e9de8
xspeeds <71892765+xploitspeeds@users.noreply.github.com>
2020-12-14 10:57:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ io.on('connection', socket => {
     if (user) {
       io.to(user.room).emit(
         'message',
-        formatMessage(botName, `${user.username} has left the chat`)
+        formatMessage(botName, `${user.username} has left the chat :(`)
       );
 
       // Send users and room info
